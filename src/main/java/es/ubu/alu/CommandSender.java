105dc4cb44c5d045a2c395739a5ddae781d31fae
RPC <rpc0027@alu.ubu.es>
2018-12-31 00:52:31 +0100

    Remove redundant abstract modifiers from interface.

--- src/main/java/es/ubu/alu/CommandSender.java
+++ src/main/java/es/ubu/alu/CommandSender.java
@@ -51,22 +51,22 @@ public interface CommandSender {
 	}
 	
 	public void turnOnLed(Color color);
-	abstract void turnOnRed();
-	abstract void turnOnGreen();
-	abstract void turnOnBlue();
-	abstract void turnOnYellow();
-	abstract void turnOnMagenta();
-	abstract void turnOnCyan();
-	abstract void turnOnWhite();
-	abstract void turnOffLEDs();
+	void turnOnRed();
+	void turnOnGreen();
+	void turnOnBlue();
+	void turnOnYellow();
+	void turnOnMagenta();
+	void turnOnCyan();
+	void turnOnWhite();
+	void turnOffLEDs();
 	
 	public void sendMessage(byte row, String message);
-	abstract void sendMessageRow0(String message);
-	abstract void sendMessageRow1(String message);
+	void sendMessageRow0(String message);
+	void sendMessageRow1(String message);
 	
 	public void adjustsPWM(PWMDevice pwmDevice, byte percentage);
-	abstract void adjustWhite(byte percentage);
-	abstract void adjustGreen(byte percentage);
-	abstract void adjustYellow(byte percentage);
-	abstract void adjustRed(byte percentage);	
+	void adjustWhite(byte percentage);
+	void adjustGreen(byte percentage);
+	void adjustYellow(byte percentage);
+	void adjustRed(byte percentage);	
 }
